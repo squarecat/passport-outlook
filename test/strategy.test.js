d07336edcf70f0d850cabc0ae87f3e8b1dc1c881
Danielle Johnson <dinkydani@gmail.com>
2019-06-17 17:53:11 +0700

    fix(strategy): added prompt and login_hint to authorizationParams

--- test/strategy.test.js
+++ test/strategy.test.js
@@ -3,56 +3,68 @@
 
 var OutlookStrategy = require('../lib/strategy');
 
-
 describe('Strategy', function() {
-    
-  var strategy = new OutlookStrategy({
+  var strategy = new OutlookStrategy(
+    {
       clientID: 'ABC123',
       clientSecret: 'secret'
     },
-    function() {});
-    
+    function() {}
+  );
+
   it('should be named windowslive', function() {
     expect(strategy.name).to.equal('windowslive');
   });
-  
 });
 
 describe('Strategy.authorizationParams', function() {
-
-  var strategy = new OutlookStrategy({
+  var strategy = new OutlookStrategy(
+    {
       clientID: 'ABC123',
       clientSecret: 'secret'
     },
-    function() {});
+    function() {}
+  );
 
   it('returns "locale" param', function() {
-    var params = { locale: "en" };
+    var params = { locale: 'en' };
     expect(strategy.authorizationParams(params)).to.deep.equal(params);
   });
 
   it('returns "display" param', function() {
-    var params = { display: "some" };
+    var params = { display: 'some' };
     expect(strategy.authorizationParams(params)).to.deep.equal(params);
   });
 
   it('returns "login_hint" param', function() {
-    var params = { loginHint: "some@email.com" };
-    expect(strategy.authorizationParams(params)).to.deep.equal({ login_hint: params.loginHint });
+    var params = { login_hint: 'some@email.com' };
+    expect(strategy.authorizationParams(params)).to.deep.equal({
+      login_hint: params.login_hint
+    });
+  });
+
+  it('returns "prompt" param', function() {
+    var params = { prompt: 'some@email.com' };
+    expect(strategy.authorizationParams(params)).to.deep.equal({
+      prompt: params.prompt
+    });
   });
 
   it('returns a combination of valid params', function() {
     var params = {
-      some: "other param",
-      display: "some",
-      loginHint: "some@email.com",
-      locale: "en",
-      invalid: "param"
+      some: 'other param',
+      display: 'some',
+      login_hint: 'some@email.com',
+      locale: 'en',
+      invalid: 'param',
+      prompt: 'login'
     };
+
     expect(strategy.authorizationParams(params)).to.deep.equal({
       display: params.display,
-      login_hint: params.loginHint,
-      locale: params.locale
+      login_hint: params.login_hint,
+      locale: params.locale,
+      prompt: params.prompt
     });
   });
 });
